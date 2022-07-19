0c989bf8ee918a762250de68c403a86506454ed9
Chandika Malimage <tmalimage@gmail.com>
2022-07-19 18:05:40 +0530

    add warmup

---
+++ intern/src/main/java/WarmUpChallenge.java
@@ -0,0 +1,11 @@
+/**
+ * @author Chandika Malimage
+ */
+public class WarmUpChallenge {
+
+    public static void main(String[] args) {
+
+        int [] sortedArray = {5, 10, 15, 20, 25};
+    }
+
+}
